95d9d6b0fad5df14654854f8469cf5dbc7d2d463
Edgar <edgartavares_99@hotmail.com>
2023-02-03 13:15:09 +0100

    Final v2

--- src/Coponent/Inscription.js
+++ src/Coponent/Inscription.js
@@ -1,45 +1,45 @@
 
-import {useNavigate } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-
 import { doc, setDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { useState } from "react";
+import { db } from "../firebase";
+
+export const handleSubmit = (event, auth, email, password, setError, db, history) => {
+  event.preventDefault();
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!emailRegex.test(email)) {
+    setError('Email is not valid');
+    return;
+  }
+  if (password.length < 8) {
+    setError('Password must be at least 8 characters');
+    return;
+  }
+  setError(null);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      setDoc(doc(db, "favorite", auth.currentUser.uid), {
+        1: false
+      });
+      signInWithEmailAndPassword(auth, email, password);
+      history("/home");
+    })
+    .catch((error) => {
+      setError(error.message);
+    });
+};
 
 function SignUpForm() {
-    const auth = getAuth();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    const db = getFirestore();
-    let history =useNavigate();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!emailRegex.test(email)) {
-      setError('Email is not valid');
-      return;
-    }
-    if (password.length < 8) {
-      setError('Password must be at least 8 characters');
-      return;
-    }
-    setError(null);
-    createUserWithEmailAndPassword(auth,email, password)
-      .then(() => {
-        setDoc(doc(db, "favorite", auth.currentUser.uid), {
-          1: false
-        });
-        signInWithEmailAndPassword(auth,email, password); 
-        history("/home");
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
+  const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  let history = useNavigate();
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={(event) => handleSubmit(event, auth, email, password, setError, db, history)}>
       <label>
         Email:
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
---
+++ src/Inscription.test.js
@@ -0,0 +1,54 @@
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { handleSubmit } from './Coponent/Inscription';
+import SignUpForm from './Coponent/Inscription';
+
+jest.mock('firebase/auth', () => ({
+  signInWithEmailAndPassword: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
+}));
+
+jest.mock('firebase/firestore', () => ({
+  doc: jest.fn(),
+  setDoc: jest.fn(),
+}));
+
+describe('handleSubmit', () => {
+  const auth = { currentUser: { uid: '123' } };
+  const setError = jest.fn();
+  const navigate = jest.fn();
+  const db = {};
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should set error if email is not valid', () => {
+    handleSubmit({ preventDefault: jest.fn() }, auth, 'invalidEmail', 'password', setError, navigate, db);
+    expect(setError).toHaveBeenCalledWith('Email is not valid');
+  });
+
+  it('should set error if password is less than 8 characters', () => {
+    handleSubmit({ preventDefault: jest.fn() }, auth, 'valid@email.com', '1234567', setError, navigate, db);
+    expect(setError).toHaveBeenCalledWith('Password must be at least 8 characters');
+  });
+
+  it('should create user and sign in if email and password are valid', async () => {
+    createUserWithEmailAndPassword.mockResolvedValueOnce();
+    signInWithEmailAndPassword.mockResolvedValueOnce();
+    setDoc.mockResolvedValueOnce();
+
+    await handleSubmit({ preventDefault: jest.fn() }, auth, 'valid@email.com', 'password123', setError, navigate, db);
+
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(auth, 'valid@email.com', 'password123');
+    expect(signInWithEmailAndPassword).toHaveBeenCalledWith(auth, 'valid@email.com', 'password123');
+    expect(setDoc).toHaveBeenCalledWith(doc(db, 'favorite', '123'), { 1: false });
+  });
+
+  it('should set error if there is a problem creating or signing in the user', async () => {
+    createUserWithEmailAndPassword.mockRejectedValueOnce({ message: 'Error' });
+
+    await handleSubmit({ preventDefault: jest.fn() }, auth, 'valid@email.com', 'password123', setError, navigate, db);
+
+  });
+});
